ac98d55fbef1025359179f571c1cd14202b15530
Mathew Wheatley <mmwheatley@csu.fullerton.edu>
2023-11-08 19:51:51 -0800

    add xcode project files

---
+++ EduConnect/EduConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduConnectApp.swift
+//  EduConnect
+//
+//  Created by Mathew Wheatley on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct EduConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
